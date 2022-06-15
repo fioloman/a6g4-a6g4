def0acfe9fede5de5cf886c45da2ace7476877c0
fioloman <107041721+fioloman@users.noreply.github.com>
2022-06-15 11:36:33 -0300

    Update ing2i.py

---
+++ funciones/ing2i.py
@@ -0,0 +1,6 @@
+#Función ing2i, para ingresar 2 valores enteros
+
+def ing2i ():
+    a= int( input("Ingrese el primer entero: "))
+    b= int( input("Ingrese el segundo entero: "))
+    return [a,b]
